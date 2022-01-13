04fc5fea9e90d2189e63ea5c2183e280d52a7d5b
ZachEnt <Zach.lovejoy2003@gmail.com>
2022-01-13 00:22:37 -0600

    Update cl_welcome.lua

--- welcome/lua/autorun/client/cl_welcome.lua
+++ welcome/lua/autorun/client/cl_welcome.lua
@@ -12,9 +12,11 @@ surface.CreateFont("welcome_48", {
 })
 
 local frameColor = Color(47, 54, 64)
-local buttonColor = Color(190, 190, 190, 200)
 local discordColor = Color(88, 101, 242)
 local scrw, scrh = ScrW(), ScrH()
+local buttonColor = Color(84, 160, 255)
+local frameColor = Color(34, 47, 62)
+local whiteColor = Color(236, 240, 241)
 
 //Function to create the derma.
 function ZACH_WELCOME.CreateMenu()
@@ -29,10 +31,10 @@ function ZACH_WELCOME.CreateMenu()
     ZACH_WELCOME.Menu:SetTitle("")
     ZACH_WELCOME.Menu:SetDraggable(false)
     ZACH_WELCOME.Menu:MakePopup(true)
+    ZACH_WELCOME.Menu:ShowCloseButton()
     ZACH_WELCOME.Menu.Paint = function(self, w, h)
-        surface.SetDrawColor(color_black)
+        surface.SetDrawColor(frameColor)
         surface.DrawRect(0, 0, w, h)
-
         
         text = markup.Parse("<font=welcome_36>Welcome, " .. ply:Nick() .. ", to<color=120,200,255> Ridge Gaming<color=255,255,255,255>!")
         text:Draw(scrw / 2, scrh * .1, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
@@ -43,9 +45,9 @@ function ZACH_WELCOME.CreateMenu()
         text = markup.Parse("<font=welcome_36>Please join the <color=120,200,255>Discord <color=255,255,255,255>for updates or more information.")
         text:Draw(scrw / 2, scrh * .5, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
 
-        fadeDown = Lerp(1 * FrameTime(), fadeDown, 1)
-        surface.SetDrawColor(0, 0, 0, fadeDown)
-        surface.DrawRect(0, 0, scrw, scrh * .7)
+        //fadeDown = Lerp(1 * FrameTime(), fadeDown, 1)
+        //surface.SetDrawColor(0, 0, 0, fadeDown)
+        //surface.DrawRect(0, 0, scrw, scrh * .7)
     end
     
     local playButton = ZACH_WELCOME.Menu:Add("DButton")
@@ -53,22 +55,47 @@ function ZACH_WELCOME.CreateMenu()
     playButton:SetSize(250, 100)
     playButton:SetText("")
     playButton:Show()
+    playButton.barStatus = 0
+    playButton.speed = 5
     playButton.Paint = function(self, w, h)
-        draw.RoundedBox(10, 0, 0, w, h, Color(80, 255, 120, 255))
-        draw.SimpleText("Play", "welcome_48", w / 2, h / 2, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+        surface.SetDrawColor(buttonColor)
+        surface.DrawRect(0, 0, w, h)
+        draw.SimpleText("Play", "welcome_48", w / 2, h / 2, whiteColor, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+
+        if (self:IsHovered()) then
+            self.barStatus = math.Clamp(self.barStatus + self.speed * FrameTime(), 0, 1)   
+        else
+            self.barStatus = math.Clamp(self.barStatus - self.speed * FrameTime(), 0, 1)  
+        end
+
+        surface.SetDrawColor(whiteColor)
+        surface.DrawRect(w / 2 - w * self.barStatus, h * .95, w * self.barStatus * 2, h * .1)
     end
     playButton.DoClick = function(self)
         ZACH_WELCOME.Menu:Remove()
     end
+    
 
     local discordButton = ZACH_WELCOME.Menu:Add("DButton")
     discordButton:SetPos(scrw / 2 - 500, scrh * .8)
     discordButton:SetSize(250, 100)
     discordButton:SetText("")
     discordButton:Show()
+    discordButton.barStatus = 0
+    discordButton.speed = 5
     discordButton.Paint = function(self, w, h)
-        draw.RoundedBox(10, 0, 0, w, h, Color(88, 101, 242, 255))
-        draw.SimpleText("Discord", "welcome_48", w / 2, h / 2, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+        surface.SetDrawColor(buttonColor)
+        surface.DrawRect(0, 0, w, h)
+        draw.SimpleText("Discord", "welcome_48", w / 2, h / 2, whiteColor, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+
+        if (self:IsHovered()) then
+            self.barStatus = math.Clamp(self.barStatus + self.speed * FrameTime(), 0, 1)   
+        else
+            self.barStatus = math.Clamp(self.barStatus - self.speed * FrameTime(), 0, 1)  
+        end
+
+        surface.SetDrawColor(whiteColor)
+        surface.DrawRect(w / 2 - w * self.barStatus, h * .95, w * self.barStatus * 2, h * .1)
     end
     discordButton.DoClick = function(self)
         gui.OpenURL("https://ridge.gg")
@@ -79,9 +106,21 @@ function ZACH_WELCOME.CreateMenu()
     aboutButton:SetSize(250, 100)
     aboutButton:SetText("")
     aboutButton:Show()
+    aboutButton.barStatus = 0
+    aboutButton.speed = 5
     aboutButton.Paint = function(self, w, h)
-        draw.RoundedBox(10, 0, 0, w, h, Color(255, 220, 120, 255))
-        draw.SimpleText("Support Us", "welcome_48", w / 2, h / 2, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+        surface.SetDrawColor(buttonColor)
+        surface.DrawRect(0, 0, w, h)
+        draw.SimpleText("Support Us", "welcome_48", w / 2, h / 2, whiteColor, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+
+        if (self:IsHovered()) then
+            self.barStatus = math.Clamp(self.barStatus + self.speed * FrameTime(), 0, 1)   
+        else
+            self.barStatus = math.Clamp(self.barStatus - self.speed * FrameTime(), 0, 1)  
+        end
+
+        surface.SetDrawColor(whiteColor)
+        surface.DrawRect(w / 2 - w * self.barStatus, h * .95, w * self.barStatus * 2, h * .1)
     end
     aboutButton.DoClick = function(self, w, h)
         gui.OpenURL("https://ridge.gg/store")
